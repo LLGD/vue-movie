41fd6cbad841f195d014c4b659066969f4634ba8
LLGD <839365141@qq.com>
2020-05-11 16:18:52 +0800

    add location

--- src/views/movie/index.vue
+++ src/views/movie/index.vue
@@ -26,12 +26,58 @@
 
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+import {messageBox} from '@/components/JS'
 
 export default {
   name:'movie',
   components:{
     Header,
-    Footer
+    Footer,
+  },
+  mounted () {
+    setTimeout(() => {
+       this.axios.get('/api/getLocation').then((res)=>{
+      let msg = res.data.msg
+
+      if(msg==='ok'){
+
+        let nm =res.data.data.nm
+        let id =res.data.data.id
+        if(this.$store.state.city.id==id){
+          return
+        }
+
+        messageBox({
+            title:'定位',
+            content:nm,
+            cancel:'取消',
+            ok:'切换定位',
+            handleOk(){
+              window.localStorage.setItem('nowNm',nm)
+              window.localStorage.setItem('nowId',id)
+              window.location.reload()
+            }
+          })
+      }
+    })
+    }, 3000);
+   
+
+    // messageBox({
+    //   title:'定位1',
+    //   content:'沈阳',
+    //   cancel:'取消',
+    //   ok:'切换定位',
+    //   handleCancel(){
+    //     console.log(1);
+        
+    //   },
+    //   handleOk(){
+    //     console.log(2);
+        
+    //   }
+
+    // })
   }
 }
 </script>
